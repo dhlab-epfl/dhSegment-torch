82ca4c8f9517aea62dd5c1108b3e459f481c9771
Raphaël Barman <raphael.barman@epfl.ch>
2020-07-31 10:53:43 +0200

    Optimized batch confusion matrix computation

--- dh_segment_torch/metrics/utils.py
+++ dh_segment_torch/metrics/utils.py
@@ -1,21 +1,51 @@
-from typing import Tuple, Optional
+from typing import Optional, Union, List
 
 import numpy as np
 import torch
 
 
 def batch_multilabel_confusion_matrix(
-    y_true: torch.Tensor, y_pred: torch.Tensor, n_classes: int, multilabel: bool
+    y_true: torch.Tensor,
+    y_pred: torch.Tensor,
+    n_classes: int,
+    multilabel: bool,
+    weights: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
-    y_true, y_pred = validate_input(y_true, y_pred, n_classes, multilabel)
-    n_classes = y_pred.shape[1]
-    num_samples = y_pred.shape[2:].numel()
+    validate_input(y_true, y_pred, n_classes, multilabel)
 
-    true_and_pred = y_true.mul(y_pred)
+    n_batches = y_pred.shape[0]
 
-    tp_sum = batch_bincount(true_and_pred, minlength=n_classes)
-    pred_sum = batch_bincount(y_pred, minlength=n_classes)
-    true_sum = batch_bincount(y_true, minlength=n_classes)
+    if multilabel:
+        true_and_pred = y_true.mul(y_pred)
+        num_samples = y_pred.shape[2:].numel()
+        tp_sum = batch_bincount(
+            batch_nonzero(true_and_pred, n_batches),
+            n_batches,
+            weights,
+            minlength=n_classes,
+        )
+        pred_sum = batch_bincount(
+            batch_nonzero(y_pred, n_batches), n_batches, weights, minlength=n_classes
+        )
+        true_sum = batch_bincount(
+            batch_nonzero(y_true, n_batches), n_batches, weights, minlength=n_classes
+        )
+    else:
+        true_and_pred = [
+            y_pred[batch_idx][y_pred[batch_idx] == y_true[batch_idx]]
+            for batch_idx in range(n_batches)
+        ]
+        num_samples = y_pred.shape[1:].numel()
+        tp_sum = batch_bincount(true_and_pred, n_batches, weights, minlength=n_classes)[
+            :, 1:
+        ]
+        pred_sum = batch_bincount(
+            y_pred.view(n_batches, -1), n_batches, weights, minlength=n_classes
+        )[:, 1:]
+        true_sum = batch_bincount(
+            y_true.view(n_batches, -1), n_batches, weights, minlength=n_classes
+        )[:, 1:]
+        n_classes -= 1
 
     fp = pred_sum - tp_sum
     fn = true_sum - tp_sum
@@ -26,64 +56,37 @@ def batch_multilabel_confusion_matrix(
 
 def validate_input(
     y_true: torch.Tensor, y_pred: torch.Tensor, n_classes: int, multilabel: bool
-) -> Tuple[torch.sparse.LongTensor, torch.sparse.LongTensor]:
+) -> None:
     if multilabel:
         assert y_true.shape[1] == n_classes
-        y_true = one_hot_to_sparse(y_true)
-        y_pred = one_hot_to_sparse(y_pred)
+
     else:
-        y_true = multiclass_to_one_hot(y_true, n_classes)
-        y_pred = multiclass_to_one_hot(y_pred, n_classes)
+        assert y_pred.max() < n_classes
+        assert y_true.max() < n_classes
 
     assert y_true.shape == y_pred.shape
     assert y_pred.ndim > 2
-    return y_true, y_pred
-
-
-def one_hot_to_sparse(one_hot: torch.Tensor) -> torch.sparse.LongTensor:
-    nonzeros = torch.nonzero(one_hot, as_tuple=False)
-    return torch.sparse.LongTensor(
-        nonzeros.t(), torch.ones(nonzeros.shape[0], dtype=torch.long), one_hot.shape
-    ).coalesce()
-
-
-def multiclass_to_one_hot(
-    multiclass: torch.Tensor, n_classes: int
-) -> torch.sparse.LongTensor:
-    if not multiclass.dtype == torch.long:
-        multiclass = multiclass.to(torch.long)
-    nonzeros = torch.nonzero(multiclass, as_tuple=False)
-    indices = torch.cat(
-        [
-            nonzeros[:, :1],
-            (multiclass[multiclass > 0] - 1).unsqueeze(1),
-            nonzeros[:, 1:],
-        ],
-        dim=-1,
-    )
-    shape = multiclass.shape[:1] + torch.Size([n_classes - 1]) + multiclass.shape[1:]
-
-    one_hot = torch.sparse.LongTensor(
-        indices.t(), torch.ones(indices.shape[0], dtype=torch.long), shape
-    ).coalesce()
-
-    return one_hot
 
 
 def batch_bincount(
-    batch_input: torch.Tensor, weights: Optional[torch.Tensor] = None, minlength=0
-):
-    if batch_input.ndim < 3:
-        raise ValueError("Expected a tensor of shape (batch, classes, ...other dims)")
-    return torch.cat(
+    batch_input: Union[torch.Tensor, List[torch.Tensor]],
+    n_batches: int,
+    weights: Optional[torch.Tensor] = None,
+    minlength=0,
+) -> torch.Tensor:
+    return torch.stack(
         [
-            torch.bincount(
-                batch_input[idx].coalesce().indices()[0], weights, minlength
-            ).unsqueeze(0)
-            for idx in range(batch_input.shape[0])
-        ],
-        dim=0,
+            torch.bincount(batch_input[batch_idx], weights=weights, minlength=minlength)
+            for batch_idx in range(n_batches)
+        ]
     )
+
+
+def batch_nonzero(batch_input: torch.Tensor, n_batches: int,) -> List[torch.Tensor]:
+    non_zeros = torch.nonzero(batch_input, as_tuple=False)
+    return [
+        non_zeros[:, 1][non_zeros[:, 0] == batch_idx] for batch_idx in range(n_batches)
+    ]
 
 
 def nanaverage(a, axis=None, weights=None):
