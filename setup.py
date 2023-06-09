79dab54dbf1806e0f103e36a19b9d971a06eed77
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2023-06-09 08:07:39 +0100

    Testing dependencies update

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 import sys
 
 if sys.platform == 'win32':
-    jsonnet = "jsonnetbin>=0.16.0"
+    jsonnet = "jsonnetbin==0.17.0"
 else:
-    jsonnet = "jsonnet>=0.16.0"
+    jsonnet = "jsonnet==0.17.0"
 
 setup(
     name="dh_segment_torch",
@@ -19,26 +19,26 @@ setup(
         "Source Code": "https://github.com/dhlab-epfl/dhSegment-torch",
     },
     install_requires=[
-        "sacred==0.8",
-        "torch==1.6.0",
-        "torchvision==0.7.0",
-        "tensorboard==2.0.0",
-        "scikit-image==0.17.2",
-        "scikit-learn==0.22.2.post1",
-        "pandas==1.2.4",
-        "numpy==1.18.1",
-        "scipy==1.3.2",
-        "networkx==2.4",
+        "sacred==0.8.1",
+        "torch==1.9.0",
+        "torchvision==0.10.1",
+        "tensorboard==2.4.1",
+        "scikit-image==0.18.1",
+        "scikit-learn==0.24.2",
+        "pandas==1.3.2",
+        "numpy==1.21.0",
+        "scipy==1.7.1",
+        "networkx==2.5.1",
         "lxml==4.6.3",
         "pretrainedmodels==0.7.4",
-        "opencv-python-headless==4.5.2.52",
+        "opencv-python-headless==4.5.3.56",
         "PyYaml==5.4.1",
-        "frozendict==2.0.2",
-        "albumentations==0.5.2",
+        "frozendict==2.0.6",
+        "albumentations==1.0.3",
         "shapely==1.7.1",
-        "tqdm==4.60.0",
-        "requests==2.25.1",
-        jsonnet
+        "tqdm==4.61.2",
+        "requests==2.26.0",
+        "jsonnet==0.17.0"
     ],
     extras_require={
         "doc": [
